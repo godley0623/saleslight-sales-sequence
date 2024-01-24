7be5408a3baea9c422db45a95ec5d92d97ca935f
godley0623 <godley11691@yahoo.com>
2024-01-24 03:47:09 -0500

    added search bar for sales table

--- app/components/SalesTable.tsx
+++ app/components/SalesTable.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { getSalesPrompts } from '../controller/server'
 
 export default function SalesTable() {
+    const [allSales, setAllSales]: any = useState([])
     const [sales, setSales]: any = useState([])
 
     useEffect(() => {
@@ -14,6 +15,7 @@ export default function SalesTable() {
             let salesPrompts = await getSalesPrompts(token, email)
             if (salesPrompts) {
                 salesPrompts = salesPrompts.sales_prompts.reverse()
+                setAllSales(salesPrompts)
                 setSales(salesPrompts)
             }
 
@@ -24,9 +26,27 @@ export default function SalesTable() {
         fetchData()
     }, [])
 
+    const handleSearch = (e:any) => {
+        const search = e.target.value
+
+        if (!search) {
+            setSales(allSales)
+            return
+        }
+
+        const salesFilter = allSales.filter((sale: any) => {
+            if (sale.lead_name.toLowerCase().includes(search.toLowerCase()) || sale.creation_date.includes(search)) return true
+            return false
+        })
+
+        if (salesFilter.length > 0) setSales(salesFilter)
+    }
+
     return (
+        <>
+        <input className='border border-black ml-2 pl-2' placeholder='Search...' onChange={(e) => handleSearch(e)}></input>
         <div className='w-full flex justify-center mt-5'>
-            <table className='border border-black' bgcolor='black' width={`${100*(sales.length)}`}>
+            <table className='border border-black' bgcolor='black' width="800">
                 <thead>
                     <tr className=' bg-gray-500'>
                         <th className='w-24'>Lead Name</th>
@@ -47,5 +67,6 @@ export default function SalesTable() {
                 </tbody>
             </table>
         </div>
+        </>
     )
 }
